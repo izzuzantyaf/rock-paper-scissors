6d45857404e0153682fd6515a2069f305736e07a
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-12-26 22:51:00 +0700

    disignore build folder

---
+++ build/precache-manifest.bd457bb661bf3c272dd07c3a097f0a7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66017335159fd47d4d10bc9268bd711f",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "3a429c3367a4a7881f4b",
+    "url": "/rock-paper-scissors/static/css/main.bf43203d.chunk.css"
+  },
+  {
+    "revision": "c19a7fd72fac20f15387",
+    "url": "/rock-paper-scissors/static/js/2.81013e43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.81013e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a429c3367a4a7881f4b",
+    "url": "/rock-paper-scissors/static/js/main.735ffee9.chunk.js"
+  },
+  {
+    "revision": "54d8b3ee6fc233b06722",
+    "url": "/rock-paper-scissors/static/js/runtime-main.5af9a2bb.js"
+  },
+  {
+    "revision": "7e32610f4975d693acb61e1fa4e070ff",
+    "url": "/rock-paper-scissors/static/media/fist.7e32610f.svg"
+  },
+  {
+    "revision": "b17e8f28f89f62d02fe94a29040fdced",
+    "url": "/rock-paper-scissors/static/media/hold.b17e8f28.svg"
+  },
+  {
+    "revision": "030b9d6201a2bf14925e43040d5ecfcb",
+    "url": "/rock-paper-scissors/static/media/victory.030b9d62.svg"
+  }
+]);
